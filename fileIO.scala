827eff150cd8d95391a28c21166d820acb3fdc54
lei li <lei.li27@yahoo.com>
2016-12-13 08:45:36 +0800

    update

--- fileIO.scala
+++ fileIO.scala
@@ -1,4 +1,5 @@
 import java.io._
+
 object Demo {
    def strDecode(str:String): String = {
 		var newStr: String = str.replace("\\n","\n")
@@ -11,13 +12,25 @@ object Demo {
 	  if(args.length>=2){ 
 		val fileName: String = args.toList(0)
 		val fileContent: String = strDecode(args.toList(1))
-		println(fileContent)
 		println("ok, start to write file!")
 		
 		val writer = new FileWriter(new File(fileName+".txt"))
 		writer.write(fileContent)
 		writer.close()
-		println("completed!")
+		
+		println("read from "+fileName+".txt")
+		scala.io.Source.fromFile(fileName+".txt" ).foreach{ 
+			print 
+		}
+		println()
+		// use java.io.FileWriter
+		val out = new java.io.FileWriter("./out.txt")
+		out.write("hello world\n\n123")
+		out.close()
+		// use scala.io.Source
+		scala.io.Source.fromFile(new java.io.File("out.txt")).getLines().foreach(println)
+		
+		println("\ncompleted!")
 		
 	  }else{
 		println("arguments are not justify!")
