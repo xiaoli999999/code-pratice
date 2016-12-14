ef5d533196f7889b7c3f320e935b4ed0b22e2042
lei li <lei li>
2016-12-14 23:40:03 +0800

    typeConvertor

--- typeConvertor.scala
+++ typeConvertor.scala
@@ -1,13 +1,39 @@
+/******
+*
+*第一个object要怎么写才能让结果等于23？
+*
+*******/
+class A(s: String){def ddd()={println(s)}}
 object ImplicitDemo {
-	def display(input: String): Unit = println(input)
-	def display2(input: Int): Unit = println(input+1)
+
+	def display(input:String):Unit = println(input)
 	implicit def typeConvertor(input:Int): String = input.toString
-	implicit def typeConvertor(input: String): Int = input.toInt
+	implicit def typeConvertor(input:Boolean):String = if(input==true) "T" else "F"
+	
+	def display2(input:Int):Unit = println(input+1)
+	implicit def typeConvertor2(input:String) = input.toInt
 	
 	def main(args: Array[String]): Unit = {
-		display("1212")
 		display(12)
-		
-		//display2("22")
+		display(true)
+		display2(1)
+		display2("1")
+		val a = new A("asd")
+		a.ddd
   }
 }
+/***********
+* error: type mismatch;
+* found   : input.type (with underlying type String)
+* required: ?{def toInt: ?}
+* Note that implicit conversions are not applicable because they are ambiguous:
+* both method augmentString in object Predef of type (x: String)scala.collection.immutable.StringOps
+* and method typeConvertor in object Main of type (input: String)Int
+* are possible conversion functions from input.type to ?{def toInt: ?}
+*        implicit def typeConvertor(input: String): Int = input.toInt
+*                                                         ^
+* error: value toInt is not a member of String
+*        implicit def typeConvertor(input: String): Int = input.toInt
+*                                                               ^
+* two errors found
+********/
